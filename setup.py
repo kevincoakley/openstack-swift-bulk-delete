990905e16747c99cd24636edeeb57d3b7552f9c4
Kevin Coakley <kcoakley@sdsc.edu>
2016-02-03 20:47:58 -0800

    Initial Project Structure

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+    extra = dict(install_requires=[
+        "requests>=2.8",
+    ],
+        include_package_data=True,
+        test_suite="tests.suite.load_tests",
+    )
+except ImportError:
+    from distutils.core import setup
+    extra = {}
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(name="openstack-swift-bulk-delete",
+      version="0.0.1",
+      description="OpenStack Swift Bulk Delete CLI",
+      long_description=readme(),
+      author="Kevin Coakley",
+      author_email="kcoakley@sdsc.edu",
+      scripts=[
+          "bin/swift-bulk-delete",
+      ],
+      url="",
+      packages=[
+          "swiftbulkdelete",
+      ],
+      package_data={
+      },
+      platforms="Posix; MacOS X",
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+      ],
+      **extra
+      )
