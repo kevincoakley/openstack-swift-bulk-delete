396f57fb3b4d134d5822ffb12e97f226461295a1
Kevin Coakley <kcoakley@sdsc.edu>
2016-02-09 14:59:57 -0800

    Added global variables

---
+++ swiftbulkdelete/variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import os
+
+os_tenant_name = os.environ.get("OS_TENANT_NAME", None)
+os_username = os.environ.get("OS_USERNAME", None)
+os_password = os.environ.get("OS_PASSWORD", None)
+os_auth_url = os.environ.get("OS_AUTH_URL", None)
