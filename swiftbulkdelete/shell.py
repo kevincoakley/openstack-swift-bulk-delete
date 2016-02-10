72584ee6f1cefc37dc07f28d371be87e0c3ac2ec
Kevin Coakley <kcoakley@sdsc.edu>
2016-02-09 19:50:33 -0800

    Return when all of the threads have died
    verify all of the env vars have been set; check the that os_auth_url
    have v1.0 in it for swauth.

--- swiftbulkdelete/shell.py
+++ swiftbulkdelete/shell.py
@@ -27,9 +27,16 @@ def main():
     logging.debug("container: %s, limit: %s, threads: %s",
                   args["container"], args["limit"], args["threads"])
 
+    if variables.os_tenant_name is None or variables.os_username is None \
+            or variables.os_password is None or variables.os_auth_url is None:
+        print "The OS_TENANT_NAME, OS_USERNAME, OS_PASSWORD, OS_AUTH_URL environment " \
+              "variables must be set!"
+        return
+
     try:
         threads = []
 
+        # Create the requested number of threads
         for i in range(int(args["threads"])):
             t = threading.Thread(target=worker, args=(args["container"], args["limit"], i,))
             t.daemon = True
@@ -37,8 +44,16 @@ def main():
             t.start()
             time.sleep(1)
 
+        # Keep looping until all of the threads have died
         while True:
-            pass
+            need_return = True
+
+            for thread in threads:
+                if thread.isAlive() is True:
+                    need_return = False
+
+            if need_return is True:
+                return
     except KeyboardInterrupt:
         return
 
@@ -48,11 +63,19 @@ def worker(container, limit, thread_num):
     count = 0
 
     try:
+        # Keep looping until either swift returns an error or bulk delete returns a bad request
+        # response
         while True:
 
+            # Get a new auth token every 20 times through the loop
             if count % 20 is 0:
-                auth = Swauth(variables.os_auth_url, variables.os_tenant_name,
-                              variables.os_username, variables.os_password)
+                # Use the Swauth class for v1.0 auth urls
+                if "v1.0" in variables.os_auth_url:
+                    auth = Swauth(variables.os_auth_url, variables.os_tenant_name,
+                                  variables.os_username, variables.os_password)
+                else:
+                    print "Invalid os_auth_url"
+                    return
                 auth.get_auth_token()
 
             swift = Swift(auth.auth_token, auth.storage_url, container, limit, thread_num)
